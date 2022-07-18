c9d591503ecc8e295c4df7f79abf740ec6245008
Rohithj47 <rohithjanardhan98@gmail.com>
2022-07-18 21:33:07 +0530

    Fix timeline API

--- controllers/postController.js
+++ controllers/postController.js
@@ -52,22 +52,14 @@ module.exports.timelinePosts = [
       if(err) { return res.status(500).json(err)}
       Post.find({userId: user._id }, async (err, posts) => {
         if(err) { return res.status(500).json(err) }
-        returnPosts.push(...posts)
-        const friendPosts = []
-        if(user.followings && user.followings.length >0){
-          friendPosts = await Promise.all (
-            user.followings.map((friendId) => {
-              return Post.find({ userId: friendId });
-            }))
-        }
-        console.log("Hi")
-        returnPosts.push(...friendPosts)
-        res.status(200).json(returnPosts)
-        
+        const friendPosts = await Promise.all(
+          user.following.map((friendId) => {
+            return Post.find({ userId: friendId });
+          })
+        );
+        returnPosts = posts.concat(friendPosts[0])
+        return res.status(200).json(returnPosts)
       })
-      // console.log("Hello lets print the output list")
-      // console.log(returnPosts)
-      // return res.status(200).json(returnPosts)
     })
   }
 ]
