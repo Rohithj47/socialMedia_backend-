6e702e3b0c85dbc38d12678ab48f0a5f3f1bc029
Rohithj47 <rohithjanardhan98@gmail.com>
2022-07-18 19:22:52 +0530

    Fix timeline API

--- controllers/postController.js
+++ controllers/postController.js
@@ -44,3 +44,30 @@ module.exports.deletePost = (req, res) => {
         }
     });
 }
+
+module.exports.timelinePosts = [
+  (req,res) => {
+    returnPosts = []
+    User.findById(req.params.userId, async (err, user) => {
+      if(err) { return res.status(500).json(err)}
+      Post.find({userId: user._id }, async (err, posts) => {
+        if(err) { return res.status(500).json(err) }
+        returnPosts.push(...posts)
+        const friendPosts = []
+        if(user.followings && user.followings.length >0){
+          friendPosts = await Promise.all (
+            user.followings.map((friendId) => {
+              return Post.find({ userId: friendId });
+            }))
+        }
+        console.log("Hi")
+        returnPosts.push(...friendPosts)
+        res.status(200).json(returnPosts)
+        
+      })
+      // console.log("Hello lets print the output list")
+      // console.log(returnPosts)
+      // return res.status(200).json(returnPosts)
+    })
+  }
+]
